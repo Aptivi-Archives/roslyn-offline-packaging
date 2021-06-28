b11a256054a6c2135d2ed946466cf2b2958ca4d3
EoflaOE <eoflaoevicecity@gmail.com>
2021-06-28 23:04:54 +0300

    Added amending

---
+++ initializeamend.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Run from root directory of linux-offline-packaging-roslyn
+
+# Source ./eng/common/tools.sh
+echo "- Sourcing ./roslyn/eng/common/tools.sh..."
+echo "  - . ./roslyn/eng/common/tools.sh"
+. ./roslyn/eng/common/tools.sh
+
+# Check for components inside ./offline folder
+echo "- Checking ./offline/ folder to see if required components exist..."
+
+# .dotnet
+echo "  - .dotnet/..."
+if [ ! -e "./offline/.dotnet" ]; then
+	echo "  - .dotnet/ not found. creating..."
+	mkdir "./offline/.dotnet"
+fi
+# .dotnet/dotnet-install.sh
+echo "  - .dotnet/dotnet-install.sh"
+if [ ! -e "./offline/.dotnet/dotnet-install.sh" ]; then
+        echo "  - .dotnet/dotnet-install.sh not found. downloading..."
+        wget --continue --output-document="./offline/.dotnet/dotnet-install.sh" "https://dot.net/v1/dotnet-install.sh"
+fi
+# .dotnet/dep/
+echo "  - .dotnet/dep/..."
+if [ ! -e "./offline/.dotnet/dep/" ]; then
+        echo "  - .dotnet/dep/ not found. creating..."
+        mkdir "./offline/.dotnet/dep"
+fi
+# .dotnet/dep/Sdk
+echo "  - .dotnet/dep/Sdk/..."
+if [ ! -e "./offline/.dotnet/dep/Sdk/" ]; then
+        echo "  - .dotnet/dep/Sdk/ not found. creating..."
+        mkdir "./offline/.dotnet/dep/Sdk"
+fi
+# .dotnet/dep/Sdk/${dotnet_sdk_version}
+ReadGlobalVersion "dotnet"
+dotnet_sdk_version=$_ReadGlobalVersion
+echo "  - .dotnet/dep/Sdk/${dotnet_sdk_version}..."
+if [ ! -e "./offline/.dotnet/dep/Sdk/${dotnet_sdk_version}/" ]; then
+        echo "  - .dotnet/dep/Sdk/${dotnet_sdk_version} not found. creating..."
+        mkdir "./offline/.dotnet/dep/Sdk/${dotnet_sdk_version}"
+fi
+
+# Check for required files
+echo "- Determining what files to download..."
+
+# .dotnet/dep/Sdk/${dotnet_sdk_version}/dotnet-sdk-${dotnet_sdk_version}-linux-x64.tar.gz
+echo "  - .dotnet/dep/Sdk/${dotnet_sdk_version}/dotnet-sdk-${dotnet_sdk_version}-linux-x64.tar.gz..."
+if [ ! -e "./offline/.dotnet/dep/Sdk/${dotnet_sdk_version}/dotnet-sdk-${dotnet_sdk_version}-linux-x64.tar.gz" ]; then
+        echo "  - dotnet-sdk-${dotnet_sdk_version}-linux-x64.tar.gz not found. downloading..."
+        wget --continue --output-document="./offline/.dotnet/dep/Sdk/${dotnet_sdk_version}/dotnet-sdk-${dotnet_sdk_version}-linux-x64.tar.gz" "https://dotnetcli.azureedge.net/dotnet/Sdk/${dotnet_sdk_version}/dotnet-sdk-${dotnet_sdk_version}-linux-x64.tar.gz"
+fi
+
+# Copy required files from offline to roslyn
+echo "- Copying .dotnet..."
+echo "  - cp -R offline/.dotnet roslyn/"
+cp -R offline/.dotnet roslyn/
+
+echo "- Build using \"./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests /p:DisableNerdbankVersioning=true\" from the \"msbuild\" directory."
+echo "- For Launchpad PPAs and general Ubuntu package builds, change \"preview\" in \"debian/changelog\" to \"focal\" or any Ubuntu codename."
+echo "- You may want to run \"dch -U\" to sign your custom msbuild package changelog."
+echo "- Use \"debuild -S -sa\" to build source package for \"sbuild\"."
+echo "- Undo patches once done."
+
